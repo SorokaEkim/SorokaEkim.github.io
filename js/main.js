6a42e8cf8f98837a13776357e6ab7553a3aedf26
SorokaEkim <74772140+SorokaEkim@users.noreply.github.com>
2021-05-06 21:57:40 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Burger
 const open    = document.querySelector('.header__burger');
       menu    = document.querySelector('.header__burger-menu');
 
@@ -7,12 +8,47 @@ open.onclick = function() {
 }
 
 // Swiper
-
 var swiper = new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+// Swiper_2
+var swiper = new Swiper(".mySwiper_2", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+  breakpoints: {
+    // when window width is <= 499px
+    320: {
+        slidesPerView: 1,
+        spaceBetweenSlides: 30
+    },
+    // when window width is <= 999px
+    640: {
+        slidesPerView: 2,
+        spaceBetweenSlides: 40
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetweenSlides: 40
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetweenSlides: 40
+    }
+}
 });
 
 
