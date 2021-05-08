a10b8c8d316970e5fb447201dbdb5ff8d9e3f6f4
SorokaEkim <74772140+SorokaEkim@users.noreply.github.com>
2021-05-08 09:35:09 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -33,23 +33,46 @@ var swiper = new Swiper(".mySwiper_2", {
     // when window width is <= 499px
     320: {
         slidesPerView: 1,
-        spaceBetweenSlides: 30
+        spaceBetweenSlides: 30,
+        allowTouchMove: true,
     },
     // when window width is <= 999px
     640: {
         slidesPerView: 2,
-        spaceBetweenSlides: 40
+        spaceBetweenSlides: 40,
+        allowTouchMove: true,
     },
     1024: {
       slidesPerView: 3,
-      spaceBetweenSlides: 40
+      spaceBetweenSlides: 40,
+      allowTouchMove: false,
     },
     1280: {
       slidesPerView: 4,
-      spaceBetweenSlides: 40
+      spaceBetweenSlides: 40,
+      allowTouchMove: false,
     }
 }
 });
+
+function addHandlers(count) {
+  var minus = count.querySelector(".quanity-down");
+  var number = count.querySelector(".quanity");
+  var plus = count.querySelector(".quanity-up");
+  plus.addEventListener("click", function() {
+    number.innerText++;
+  });
+  minus.addEventListener("click", function() {
+    if(number.innerText < 2) {
+      number.innerText = 1;
+    } else {
+      number.innerText--;
+    }
+  });
+}  
+
+var counts = document.querySelectorAll(".product__input");
+counts.forEach(addHandlers);
 
 
 
