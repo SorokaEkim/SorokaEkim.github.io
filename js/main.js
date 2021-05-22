5747bc82fa010e1d5148d611c8c0a5449ba450df
SorokaEkim <74772140+SorokaEkim@users.noreply.github.com>
2021-05-22 13:58:55 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -256,6 +256,43 @@ openFilter.onclick = function() {
   menuFilter.classList.toggle('hidden');
 };      
 
+// Фильтр цен
+
+const rangeSlider = document.getElementById('range-slider');
+      input0      = document.getElementById('input-0');
+      input1      = document.getElementById('input-1');
+      inputs      = [input0, input1];
+
+if (rangeSlider) {
+  noUiSlider.create(rangeSlider, {
+    start: [100, 10000],
+    step: 1,
+    connect: true,
+    range: {
+        'min': [100],
+        'max': [10000]
+    }
+  });
+
+  rangeSlider.noUiSlider.on('update', function(values, handle){
+    inputs[handle].value = Math.round(values[handle]);
+  });
+
+  const setRangeSlider = (i, value) => {
+    let arr = [null, null];
+    arr[i] = value;
+    
+    rangeSlider.noUiSlider.set(arr)
+  }
+
+  inputs.forEach((el, index) => {
+    el.addEventListener('change', (e) => {
+      setRangeSlider(index, e.currentTarget.value);
+    })
+  })
+
+}
+
 
 
 
