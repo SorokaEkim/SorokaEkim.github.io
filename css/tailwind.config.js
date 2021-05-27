011965103408a92cb059f592659c3d90d68b7f59
SorokaEkim <74772140+SorokaEkim@users.noreply.github.com>
2021-05-27 08:57:32 +0300

    Add files via upload

--- css/tailwind.config.js
+++ css/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['../**/*.js', '../**/*.html']
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
