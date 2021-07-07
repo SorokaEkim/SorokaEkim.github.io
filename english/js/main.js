6605f7cdb9cb72cf4db253e20d9f61e63e7c118e
SorokaEkim <74772140+SorokaEkim@users.noreply.github.com>
2021-07-07 10:44:20 +0300

    Add files via upload

--- english/js/main.js
+++ english/js/main.js
@@ -30,7 +30,7 @@ var swiper = new Swiper(".mySwiper", {
     // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      spaceBetween: 0
+      spaceBetween: 30
     },
     // when window width is >= 480px
     1024: {
@@ -58,6 +58,7 @@ for(let anchor of anchors) {
     })
   })
 }
+
 
 
 
