ba9bce2d578e98188bf16b2391d7781797b48200
SorokaEkim <74772140+SorokaEkim@users.noreply.github.com>
2021-11-07 18:44:32 +0300

    Add files via upload

--- ekim.project/js/main.js
+++ ekim.project/js/main.js
@@ -33,3 +33,23 @@ var swiper = new Swiper(".mySwiper", {
   effect: 'cube',
   speed: 1000,
 });
+
+// NAVIGATION //
+// Navigations
+const anchors = document.querySelectorAll('a[href^="#"]')
+
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    
+    e.preventDefault()
+    menuMobie.classList.remove('show');
+    content.classList.remove('content-blur')
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
