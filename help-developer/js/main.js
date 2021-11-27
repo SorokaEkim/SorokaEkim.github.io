668e3ddf72448f4597f69363c02727bb1bf20cf9
SorokaEkim <74772140+SorokaEkim@users.noreply.github.com>
2021-11-27 18:32:52 +0300

    Add files via upload

---
+++ help-developer/js/main.js
@@ -0,0 +1,12 @@
+// Burger
+const menuBtn = document.querySelector('.header__burger');
+let menuOpen  = false;
+menuBtn.addEventListener('click', () => {
+	if(!menuOpen) {
+		menuBtn.classList.add('open');
+		menuOpen = true;
+	} else {
+		menuBtn.classList.remove('open');
+		menuOpen = false;
+	}
+});
