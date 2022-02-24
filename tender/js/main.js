e3cd6c7758f42c3baf8fd21b50950f7b4b2b0add
SorokaEkim <74772140+SorokaEkim@users.noreply.github.com>
2022-02-25 00:13:37 +0300

    add Animations Header

---
+++ tender/js/main.js
@@ -0,0 +1,13 @@
+// Animations
+
+// Header
+gsap.from(".header__description", {duration: 1, y:400});
+gsap.from(".header__title", {duration: 1, opacity: 0, delay: 1});
+gsap.from(".header__btn-left", {duration: 1.1, x:-500});
+gsap.from(".header__btn-right", {duration: 1.1, x:500});
+
+let tl = gsap.timeline({delay: 1.2});
+
+tl.from(".block-1", {duration: 0.2, opacity: 0})
+  .from(".block-2", {duration: 0.2, opacity: 0})
+  .from(".block-3", {duration: 0.2, opacity: 0});
