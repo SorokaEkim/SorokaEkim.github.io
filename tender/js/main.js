8c16b093d7e86b77d130d4475c110cafb0f17ab1
SorokaEkim <74772140+SorokaEkim@users.noreply.github.com>
2022-02-26 00:40:16 +0300

    adaptive header

--- tender/js/main.js
+++ tender/js/main.js
@@ -8,6 +8,21 @@ gsap.from(".header__btn-right", {duration: 1.1, x:500});
 
 let tl = gsap.timeline({delay: 1.2});
 
-tl.from(".block-1", {duration: 0.2, opacity: 0})
-  .from(".block-2", {duration: 0.2, opacity: 0})
-  .from(".block-3", {duration: 0.2, opacity: 0});
+tl.to(".block-1", {duration: 0.2, opacity: 1})
+  .to(".block-2", {duration: 0.2, opacity: 1})
+  .to(".block-3", {duration: 0.2, opacity: 1});
+
+// Mobile Menu
+
+const menuBtn   = document.querySelector('.header__burger');
+let menuOpen    = false;
+
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+})
